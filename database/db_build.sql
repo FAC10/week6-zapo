0a9ec63089734746612dc178eb31beb55ef5c0cf
ConchobarMacNessa <alice.binghamcarr@gmail.com>
2017-03-30 10:59:35 +0100

    update db_build.sql eith insert into values relates #10

--- database/db_build.sql
+++ database/db_build.sql
@@ -21,3 +21,21 @@ CREATE TABLE cuisine (
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(30) NOT NULL
 )
+
+INSERT INTO recipes (title, body, cuisine_id) VALUES
+('Pineapple cake', '2 cups self-raising flour, sifted
+1 cup caster sugar
+1 can (440g) crushed pineapple in syrup or juice, undrained
+Preheat oven to 180°C. Line a loaf tin with baking paper and set aside.
+Mix all ingredients together until well combined.
+Pour into the loaf tin and bake for 55-60 minutes.', 1);
+
+INSERT INTO ingredients (name) VALUES
+('self-raising flour'),
+('tinned pineapple'),
+('caster sugar');
+
+INSERT INTO cuisine (name) VALUES
+('Baking');
+
+COMMIT;
