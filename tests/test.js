85b207670a353bc86f66805ad7e2c22ac3ae1771
oliverjam <oliverjamesphillips@gmail.com>
2017-03-30 11:54:55 +0100

    Remove unnecessary failing test
    Relates #11

--- tests/test.js
+++ tests/test.js
@@ -1,12 +1,7 @@
 const test = require('tape');
-const server = require('../src/server');
+// const server = require('../src/server');
 
 test('A test test', (t) => {
   t.equal(1, 1, '1 should equal 1');
-  t.end();
-});
-
-test('Server should return 1', (t) => {
-  t.equal(server, 1, 'Should be 1');
   t.end();
 });
