223de25db2bda600438f52015a16906ad301e1ac
oliverjam <oliverjamesphillips@gmail.com>
2017-03-30 11:42:54 +0100

    Wrote server, router & handlers
    Relates #11

---
+++ src/handlers/handlers.js
@@ -0,0 +1,41 @@
+const fs = require('fs');
+
+const handlers = {};
+
+const extensionType = {
+  html: 'text/html',
+  css: 'text/css',
+  js: 'application/javascript',
+  ico: 'image/x-icon',
+};
+
+handlers.home = (req, res) => {
+  fs.readFile(`${__dirname}/../../public/index.html`, (err, file) => {
+    if (err) {
+      res.writeHead(500, { 'Content-Type': 'text/html' });
+      res.end(`<h1>${err.message}, sorry!</h1>`);
+    }
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end(file);
+  });
+};
+
+handlers.public = (req, res) => {
+  const endpoint = req.url;
+  fs.readFile(`${__dirname}/../..${endpoint}`, (err, file) => {
+    if (err || endpoint.includes('..')) {
+      res.writeHead(500, { 'Content-Type': 'text/html' });
+      res.end('<h1>500 Server Error, sorry!</h1>');
+    }
+    const extension = endpoint.split('.')[1];
+    res.writeHead(200, { 'Content-Type': extensionType[extension] });
+    res.end(file);
+  });
+};
+
+handlers.notFound = (request, response) => {
+  response.writeHead(404, { 'Content-Type': 'text/html' });
+  response.end('<h1>404 Page Requested Cannot be Found</h1>');
+};
+
+module.exports = handlers;
--- src/server.js
+++ src/server.js
@@ -1,1 +1,9 @@
-module.exports = 1;
+const http = require('http');
+const router = require('./router');
+
+const port = process.env.PORT || 3000;
+
+const server = http.createServer(router);
+server.listen(port);
+
+console.log(`Server running on http://localhost:${port}`);
